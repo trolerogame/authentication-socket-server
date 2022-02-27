337238e238b9ca596a7526afc71d66f70fd54f83
Daniel Campos <danielgcampos.04@gmail.com>
2022-02-27 16:24:09 -0300

    delete hello world to test

--- src/index.ts
+++ src/index.ts
@@ -35,9 +35,6 @@ app.use(cors({
 app.post('/group/create',auth, createGroup)
 app.get('/group/', getGroups)
 app.get('/group/:id',auth, getGroup)
-app.get('/',(req,res) => {
-    res.send('hellooooo')
-})
 
 // socket
 
